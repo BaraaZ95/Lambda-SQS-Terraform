4573737ede8130ee1dd682067369bbd45ae882c2
Baraa-Chilldays <archangle.650@gmail.com>
2024-09-26 16:57:58 +0400

    Add AWS provider, Terraform config, and IAM role for Lambda in main.tf

--- main.tf
+++ main.tf
@@ -4,6 +4,16 @@ provider "aws" {
 
 terraform {
   required_version = ">= 1.0.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+    archive = {
+      source  = "hashicorp/archive"
+      version = "~> 2.0"
+    }
+  }
   cloud {
     organization = "Debugging"
     workspaces {
